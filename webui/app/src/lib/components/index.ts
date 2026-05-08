5c1dfa29b1a3c028deb0d2317f5518a2b8f21d7b
Adam Tauber <asciimoo@gmail.com>
2026-05-08 11:12:53 +0200

    [fix] remove unused preview popup

--- webui/app/src/lib/components/index.ts
+++ webui/app/src/lib/components/index.ts
@@ -1,5 +1,4 @@
 export { default as StatusMessage } from './StatusMessage.svelte';
 export { default as SiteHeader } from './SiteHeader.svelte';
 export { default as SiteFooter } from './SiteFooter.svelte';
-export { default as PreviewPopup } from './PreviewPopup.svelte';
 export { default as PreviewPanel } from './PreviewPanel.svelte';
